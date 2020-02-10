d13e99ed99ad661480b67d58ceb610536da67de6
Flourick <simon.gebauer@seznam.cz>
2020-02-10 22:08:10 +0100

    Coloring done

---
+++ src/main/java/flour/fmc/ColorMeTabCompleter.java
@@ -0,0 +1,34 @@
+package flour.fmc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.util.StringUtil;
+
+/**
+ * colorme command TAB completion
+ * 
+ * @author Flourick
+ */
+public class ColorMeTabCompleter implements TabCompleter
+{
+	private static final String[] FIRST_ARGS = { "list", "dark_red", "red", "gold", "yellow", "dark_green", "green", "aqua", "dark_aqua", "dark_blue", "blue", "light_purple", "dark_purple", "dark_gray", "gray", "white"};
+	
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args)
+	{
+        final List<String> completions = new ArrayList<>();
+        
+		for(String arg : FIRST_ARGS) {
+			if(arg.toLowerCase().startsWith(args[0].toLowerCase())) {
+				completions.add(arg);
+			}
+		}
+		
+        return completions;
+	}
+	
+}
--- src/main/java/flour/fmc/FMC.java
+++ src/main/java/flour/fmc/FMC.java
@@ -1,11 +1,14 @@
 package flour.fmc;
 
 import io.papermc.lib.PaperLib;
-import org.bukkit.Bukkit;
+import net.md_5.bungee.api.ChatColor;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -15,18 +18,48 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class FMC extends JavaPlugin
 {
+	private static FMC instance;
+	
+	public static FMC getInstance() {
+		return instance;
+	}
+	
 	@Override
 	public void onEnable()
 	{
+		instance = this;
 		PaperLib.suggestPaper(this);
+		
+		// Creates config.yml if not already present
+		this.saveDefaultConfig();
+		
+		this.getCommand("colorme").setTabCompleter(new ColorMeTabCompleter());
+		this.getCommand("cme").setTabCompleter(new ColorMeTabCompleter());
+		
+		 // Player Joins
+		getServer().getPluginManager().registerEvents(new Listener() {
+			@EventHandler
+			public void playerJoin(PlayerJoinEvent event)
+			{
+		 		Player player = event.getPlayer();
+				
+				// Color player when he joins
+				String color = FMC.getInstance().getConfig().getString(player.getName());
+				if(color != null) {
+					FMC.getInstance().colorPlayer(player, color);
+				}
+			}
+		}, this);
 	}
 
 	@Override
 	public void onDisable()
 	{
+		saveConfig();
 		getLogger().info("FMC plugin has been disabled");
 	}
 	
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String args[])
 	{
 		if(commandLabel.equalsIgnoreCase("colorme") || commandLabel.equalsIgnoreCase("cme")){
@@ -44,74 +77,103 @@ public class FMC extends JavaPlugin
 			if(player.hasPermission("fmc.colorme")) {
 				if(args[0].equalsIgnoreCase("list")) {
 					player.sendMessage(new String[] {
-							"Available colors: ",
-							  "§4dark_red§r, "
-							+ "§cred§r, "
-							+ "§6gold§r, "
-							+ "§eyellow§r, "
-							+ "§2dark_green§r, "
-							+ "§agreen§r, "
-							+ "§baqua§r, "
-							+ "§3dark_aqua§r, "
-							+ "§1dark_blue§r, "
-							+ "§9blue§r, "
-							+ "§dlight_purple§r, "
-							+ "§5dark_purple§r, "
-							+ "§8dark_gray§r, "
-							+ "§7gray§r, "
-							+ "§fwhite"
+						"Available colors: ",
+						  "§4dark_red§r, "
+						+ "§cred§r, "
+						+ "§6gold§r, "
+						+ "§eyellow§r, "
+						+ "§2dark_green§r, "
+						+ "§agreen§r, "
+						+ "§baqua§r, "
+						+ "§3dark_aqua§r, "
+						+ "§1dark_blue§r, "
+						+ "§9blue§r, "
+						+ "§dlight_purple§r, "
+						+ "§5dark_purple§r, "
+						+ "§8dark_gray§r, "
+						+ "§7gray§r, "
+						+ "§fwhite"
 					});
 				}
 				else {
 					switch(args[0]) {
 						case "dark_red":
-							
+							player.setDisplayName(ChatColor.DARK_RED + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.DARK_RED + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "red":
-							
+							player.setDisplayName(ChatColor.RED + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.RED + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "gold":
-							
+							player.setDisplayName(ChatColor.GOLD + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.GOLD + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "yellow":
-							
+							player.setDisplayName(ChatColor.YELLOW + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.YELLOW + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "dark_green":
-							
+							player.setDisplayName(ChatColor.DARK_GREEN + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.DARK_GREEN + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "green":
-							
+							player.setDisplayName(ChatColor.GREEN + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.GREEN + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "aqua":
-							
+							player.setDisplayName(ChatColor.AQUA + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.AQUA + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "dark_aqua":
-							
+							player.setDisplayName(ChatColor.DARK_AQUA + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.DARK_AQUA + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "dark_blue":
-							
+							player.setDisplayName(ChatColor.DARK_BLUE + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.DARK_BLUE + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "blue":
-							
+							player.setDisplayName(ChatColor.BLUE + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.BLUE + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "light_purple":
-							
+							player.setDisplayName(ChatColor.LIGHT_PURPLE + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.LIGHT_PURPLE + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "dark_purple":
-							
+							player.setDisplayName(ChatColor.DARK_PURPLE + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.DARK_PURPLE + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "dark_gray":
-							
+							player.setDisplayName(ChatColor.DARK_GRAY + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.DARK_GRAY + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "gray":
-							
+							player.setDisplayName(ChatColor.GRAY + player.getName() + ChatColor.RESET);
+							player.setPlayerListName(ChatColor.GRAY + player.getName() + ChatColor.RESET);
+							this.getConfig().set(player.getName(), args[0]);
 							break;
 						case "white":
-							
+							player.setDisplayName(player.getName());
+							player.setPlayerListName(player.getName());
+							this.getConfig().set(player.getName(), null);
 							break;
-							
 						default:
-							player.sendMessage("§cInvalid arguments!");
+							player.sendMessage("§cInvalid argument " + "\'" + args[0] + "\'" + ".");
 					}
 				}
 			}
@@ -120,5 +182,71 @@ public class FMC extends JavaPlugin
 		}
 		
 		return false;
+	}
+	
+	private void colorPlayer(Player player, String color)
+	{
+		switch(color) {
+			case "dark_red":
+				player.setDisplayName(ChatColor.DARK_RED + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.DARK_RED + player.getName() + ChatColor.RESET);
+				break;
+			case "red":
+				player.setDisplayName(ChatColor.RED + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.RED + player.getName() + ChatColor.RESET);
+				break;
+			case "gold":
+				player.setDisplayName(ChatColor.GOLD + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.GOLD + player.getName() + ChatColor.RESET);
+				break;
+			case "yellow":
+				player.setDisplayName(ChatColor.YELLOW + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.YELLOW + player.getName() + ChatColor.RESET);
+				break;
+			case "dark_green":
+				player.setDisplayName(ChatColor.DARK_GREEN + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.DARK_GREEN + player.getName() + ChatColor.RESET);
+				break;
+			case "green":
+				player.setDisplayName(ChatColor.GREEN + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.GREEN + player.getName() + ChatColor.RESET);
+				break;
+			case "aqua":
+				player.setDisplayName(ChatColor.AQUA + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.AQUA + player.getName() + ChatColor.RESET);
+				break;
+			case "dark_aqua":
+				player.setDisplayName(ChatColor.DARK_AQUA + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.DARK_AQUA + player.getName() + ChatColor.RESET);
+				break;
+			case "dark_blue":
+				player.setDisplayName(ChatColor.DARK_BLUE + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.DARK_BLUE + player.getName() + ChatColor.RESET);
+				break;
+			case "blue":
+				player.setDisplayName(ChatColor.BLUE + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.BLUE + player.getName() + ChatColor.RESET);
+				break;
+			case "light_purple":
+				player.setDisplayName(ChatColor.LIGHT_PURPLE + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.LIGHT_PURPLE + player.getName() + ChatColor.RESET);
+				break;
+			case "dark_purple":
+				player.setDisplayName(ChatColor.DARK_PURPLE + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.DARK_PURPLE + player.getName() + ChatColor.RESET);
+				break;
+			case "dark_gray":
+				player.setDisplayName(ChatColor.DARK_GRAY + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.DARK_GRAY + player.getName() + ChatColor.RESET);
+				break;
+			case "gray":
+				player.setDisplayName(ChatColor.GRAY + player.getName() + ChatColor.RESET);
+				player.setPlayerListName(ChatColor.GRAY + player.getName() + ChatColor.RESET);
+				break;
+			case "white":
+				player.setDisplayName(player.getName());
+				player.setPlayerListName(player.getName());
+				break;
+		}
 	}
 }
